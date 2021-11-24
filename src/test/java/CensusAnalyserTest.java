3fde103891a8ab176d9e76fb1c50aecb9ef340af
NADEEMSHAIKH117 <nadeemshaikh1171998@gmail.com>
2021-11-24 12:05:06 +0530

    UC1 added

---
+++ src/test/java/CensusAnalyserTest.java
@@ -0,0 +1,18 @@
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
+public class CensusAnalyserTest {
+	private String INDIAN_CENSUS_CSV_FILE_PATH="C:\\Users\\Nadeem Shaikh\\eclipse-workspace\\India_States_Census_Day29\\src\\main\\resources\\IndiaStateCensusData.csv";
+
+	 @Test
+	    public void givenIndianCensusCSVFile_WhenLoad_ShouldReturnCorrectRecords() {
+	        CensusAnalyser censusAnalyser = new CensusAnalyser();
+	        int count = censusAnalyser.loadIndiaCensusData(INDIAN_CENSUS_CSV_FILE_PATH);
+	        Assert.assertEquals(29,count);
+	    }
+	
+}
