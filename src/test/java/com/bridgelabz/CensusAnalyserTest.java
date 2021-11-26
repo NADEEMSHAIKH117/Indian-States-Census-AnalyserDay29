f1a1a8bcf2599c24493349d36fa73e6bdadec35c
NADEEMSHAIKH117 <nadeemshaikh1171998@gmail.com>
2021-11-26 13:52:45 +0530

    added

--- src/test/java/com/bridgelabz/CensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/CensusAnalyserTest.java
@@ -4,7 +4,9 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import static com.bridgelabz.CensusAnalyserException.ExceptionType.CENSUS_INCORRECT_FILE_FORMAT;;
+import static com.bridgelabz.CensusAnalyserException.ExceptionType.CENSUS_INCORRECT_FILE_FORMAT;
+
+import java.io.IOException;;
 
 public class CensusAnalyserTest {
     private String INDIAN_CENSUS_CSV_FILE_PATH = "C:\\Users\\Nadeem Shaikh\\eclipse-workspace\\India_States_Census_Day29\\src\\main\\resources\\IndianStateCensusData.csv";
@@ -12,6 +14,7 @@ public class CensusAnalyserTest {
     private String INDIAN_CENSUS_INCORRECT_FILE_FORMAT = "C:\\Users\\Nadeem Shaikh\\eclipse-workspace\\India_States_Census_Day29\\src\\main\\resources\\IndianStateCensusData.txt";
     private String INDIAN_CENSUS_WITHWRONG_DELIMITER = "C:\\Users\\Nadeem Shaikh\\eclipse-workspace\\India_States_Census_Day29\\src\\main\\resources\\IndianStateCensusData.csv";
     private String INDIAN_CENSUS_WITH_WRONG_HEADER = "C:\\Users\\Nadeem Shaikh\\eclipse-workspace\\India_States_Census_Day29\\src\\main\\resources\\IndianStateCensusDataWithWrongHeader.csv";
+    private String INDIAN_STATE_CODE_CSV_FILE = "C:\\\\Users\\Nadeem Shaikh\\eclipse-workspace\\India_States_Census_Day29\\src\\main\\resources\\IndianStateCensusData.csv";
     // Tc1.1
     @Test
     public void givenIndianCensusCSVFile_WhenLoad_ShouldReturnCorrectRecords() throws CensusAnalyserException {
@@ -79,5 +82,12 @@ public class CensusAnalyserTest {
             Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_WRONG_DELIMITER, e.type);
             e.printStackTrace();
         }
+    }
+    // Tc2.1
+    @Test
+    public void givenIndianStateCodeCSVFile_WhenLoad_ShouldReturnCorrectRecords() throws IOException {
+        CensusAnalyser censusAnalyser = new CensusAnalyser();
+        int count = censusAnalyser.loadIndianStateCodeData(INDIAN_STATE_CODE_CSV_FILE);
+        Assert.assertEquals(37,count);
     }
 }
